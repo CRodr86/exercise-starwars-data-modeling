2dc35dda4cc1c354eec17bf654d59ca892c88dd6
CRodr86 <rodrigocfotografia@gmail.com>
2021-12-15 16:41:20 +0000

    Update models.py

---
+++ src/models.py
@@ -0,0 +1,41 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+from eralchemy import render_er
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    name = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    favorite_character = Column(String(250), ForeignKey('character.id'))
+    favorite_planet = Column(String(250), ForeignKey('planet.id'))
+
+class Planet(Base):
+    __tablename__ = 'planet'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    planet_name = Column(String(250), nullable=False)
+    favorited_by = Column(String(250), ForeignKey('user.id'))
+
+class Character(Base):
+    __tablename__ = "character"
+    id = Column(Integer, primary_key=True)
+    character_name = Column(String(250), nullable=False)
+    favorited_by = Column(String(250), ForeignKey('user.id'))
+
+    def to_dict(self):
+        return {}
+
+## Draw from SQLAlchemy base
+render_er(Base, 'diagram.png')
