b875416baafc063b2d56efb9d3efb611709ad431
CRodr86 <rodrigocfotografia@gmail.com>
2021-12-15 17:01:34 +0000

    Update comments

--- src/models.py
+++ src/models.py
@@ -8,11 +8,9 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-<<<<<<< HEAD
+## User Table
 class User(Base):
     __tablename__ = 'user'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     name = Column(String(250), nullable=False)
@@ -21,38 +19,19 @@ class User(Base):
     favorite_character = Column(String(250), ForeignKey('character.id'))
     favorite_planet = Column(String(250), ForeignKey('planet.id'))
 
+##Planet Table
 class Planet(Base):
     __tablename__ = 'planet'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     planet_name = Column(String(250), nullable=False)
     favorited_by = Column(String(250), ForeignKey('user.id'))
 
+##Character Table
 class Character(Base):
     __tablename__ = "character"
     id = Column(Integer, primary_key=True)
     character_name = Column(String(250), nullable=False)
     favorited_by = Column(String(250), ForeignKey('user.id'))
-=======
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
->>>>>>> edbb93e5713a62e68471482e47d87ca781e5325e
 
     def to_dict(self):
         return {}
