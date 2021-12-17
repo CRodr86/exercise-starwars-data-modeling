08ed90681ea0d85b4940eb1d0a20155982ed550b
CRodr86 <rodrigocfotografia@gmail.com>
2021-12-17 18:40:36 +0000

    final corrections

--- src/models.py
+++ src/models.py
@@ -17,21 +17,21 @@ class User(Base):
     username = Column(String(250), nullable=False)
     password = Column(String(250), nullable=False)
     favorite_character = Column(String(250), ForeignKey('character.id'))
-    favorite_planet = Column(String(250), ForeignKey('planet.id'))
+    favorite_planet = Column(Integer, ForeignKey('planet.id'))
 
 ##Planet Table
 class Planet(Base):
     __tablename__ = 'planet'
     id = Column(Integer, primary_key=True)
     planet_name = Column(String(250), nullable=False)
-    favorited_by = Column(String(250), ForeignKey('user.id'))
+    favorited_by = Column(Integer, ForeignKey('user.id'))
 
 ##Character Table
 class Character(Base):
     __tablename__ = "character"
     id = Column(Integer, primary_key=True)
     character_name = Column(String(250), nullable=False)
-    favorited_by = Column(String(250), ForeignKey('user.id'))
+    favorited_by = Column(Integer, ForeignKey('user.id'))
 
     def to_dict(self):
         return {}
